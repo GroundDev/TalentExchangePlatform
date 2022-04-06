fad37e467194ce4a0b6b8e30496d62d8825950b9
Chanhee Jeong <chan420420@gmail.com>
2022-04-07 00:56:00 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#  Talent Exchange Platform
+
+### 1. Introduction
+- 재능교환플랫폼
+
---
+++ TalentExchange/TalentExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalentExchangeApp.swift
+//  TalentExchange
+//
+//  Created by Chanhee Jeong on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct TalentExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
